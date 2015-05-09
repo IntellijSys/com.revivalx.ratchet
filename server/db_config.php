3fba024246a68501197b29cbf471a3da5b3878d6
Mohammad Nurdin <nurdinnorazanservices@gmail.com>
2015-05-09 18:19:45 +0800

    Add server.

---
+++ server/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "revivalx_todo"); // db user
+define('DB_PASSWORD', "204815"); // db password (mention your db password here)
+define('DB_DATABASE', "revivalx_todolist"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
